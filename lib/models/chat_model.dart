7f0f1c2413172d3e1089fb594956e2815a9bd50a
presheda <preciousedafe15@gmail.com>
2020-04-19 15:26:53 +0100

    Completed the whatsApp clone(Only Chat Screen)

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,45 @@
+class ChatModel {
+  final String name, message, time, avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+
+}
+
+  List<ChatModel> dummyData = [
+    new ChatModel(
+        name: "Edafe Precious",
+        message: "Hey flutter, You are so amazing !",
+        time: "15:30",
+        avatarUrl: "https://static.accessonline.com/uploads/237560.jpg"),
+
+    new ChatModel(
+        name: "Harvey Spector",
+        message: "hey i have hacked whatsapp!",
+        time: "17:20",
+        avatarUrl: "https://static.accessonline.com/uploads/237560.jpg"),
+
+    new ChatModel(
+        name: "Oladele Seyi",
+        message: "This is a pretty good framework!",
+        time: "12:10",
+        avatarUrl: "https://static.accessonline.com/uploads/237560.jpg"),
+
+    new ChatModel(
+        name: "Rachel",
+        message: "What are you up to!",
+        time: "15:30",
+        avatarUrl: "https://static.accessonline.com/uploads/237560.jpg"),
+
+    new ChatModel(
+        name: "Baren Allen",
+        message: "I'm good!",
+        time: "10:08",
+        avatarUrl: "https://static.accessonline.com/uploads/237560.jpg"),
+
+    new ChatModel(
+        name: "Balikis",
+        message: "That chubby thing",
+        time: "5:30",
+        avatarUrl: "https://static.accessonline.com/uploads/237560.jpg"),
+  ];
+
